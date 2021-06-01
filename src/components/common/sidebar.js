08e0935a8e02894c52fa90090d48ee990b1a8a1c
Hadi Tariq <hadi-tariq@live.co.uk>
2021-06-01 14:38:18 +0500

    update: sidebar language added

--- src/components/common/sidebar.js
+++ src/components/common/sidebar.js
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import SidebarItem from "../sidebarItem";
 import {FaSearch, FaCarAlt, FaTools, FaPhoneAlt, FaSignLanguage} from "react-icons/fa";
 
 const listItems = [
   {
-    iconName: () => <FaSearch className={"fontStyle"} />,
+    iconName: () => <FaSearch className={"fontStyle"}/>,
     title: "Search",
   },
   {
-    iconName: () => <FaCarAlt className={"fontStyle"} />,
+    iconName: () => <FaCarAlt className={"fontStyle"}/>,
     title: "Service",
   },
   {
-    iconName: () => <FaTools className={"fontStyle"} />,
+    iconName: () => <FaTools className={"fontStyle"}/>,
     title: "Tuning",
   },
   {
-    iconName: () => <FaPhoneAlt className={"fontStyle"} />,
+    iconName: () => <FaPhoneAlt className={"fontStyle"}/>,
     title: "Contact Us",
   },
-  {
-    iconName: () => <FaSignLanguage className={"fontStyle"} />,
-    title: "lang",
-  },
+  // {
+  //   iconName: () => <FaSignLanguage className={"fontStyle"} />,
+  //   title: "lang",
+  // },
 ];
 
 const Sidebar = () => {
+  const [activeLanguage, setActiveLanguage] = useState("US");
+  let USLanguageClass = activeLanguage === "US" ? "active" : "";
+  let ARLanguageClass = activeLanguage === "AR" ? "active" : "";
+  
   return (
     <div className={"sideBarContainer"}>
       {listItems.map((item, idx) => <SidebarItem
@@ -34,6 +38,18 @@ const Sidebar = () => {
         icon={item.iconName()}
         title={item.title}
       />)}
+      <div className={"sidebarLanguageContainer"}>
+        <button
+          className={USLanguageClass}
+          onClick={() => setActiveLanguage("US")}>
+          US
+        </button>
+        <button
+          className={ARLanguageClass}
+          onClick={() => setActiveLanguage("AR")}>
+          AR
+        </button>
+      </div>
     </div>
   );
 };
