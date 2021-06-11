3c80d97ff1d5d3743974b3d1536a5a4a6b06f7f6
Hadi Tariq <hadi-tariq@live.co.uk>
2021-06-11 19:09:54 +0500

    update: maps added

--- src/screens/FindDealers.js
+++ src/screens/FindDealers.js
@@ -1,13 +1,23 @@
 import React from "react";
 import FindDealerSearchSection from "../components/FindDealerSearchSection";
+import GoogleMapReact from "google-map-react";
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const FindDealers = () => {
   return (
     <div className={"findDealersContainer"}>
       <div className={"sectionColumn"}>
-        <FindDealerSearchSection/>
+        <FindDealerSearchSection />
       </div>
-      <div className={"sectionColumn"}>hello world</div>
+      <div className={"sectionColumn"}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyB5ukZgNnlKRZDvfM6LV2y6l9HikfZQCb8" }}
+          defaultCenter={{ lat: 59.95, lng: 30.33 }}
+          defaultZoom={1}
+        >
+          <AnyReactComponent lat={59.955413} lng={30.337844} text='My Marker' />
+        </GoogleMapReact>
+      </div>
     </div>
   );
 };
